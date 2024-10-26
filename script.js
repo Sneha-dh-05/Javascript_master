54ae398675cf4dbdc2e2e0cbc0885fdad313ae16
Sneha Dhingra <snehadhingra0987@gmail.com>
2024-10-26 13:51:30 +0530

    d1

---
+++ script.js
@@ -0,0 +1,172 @@
+// //variable and naming convention
+// let sneha_dhingra="SD";
+// console.log(sneha_dhingra)
+
+// let myfirstJob ="Software Engineering";
+// let myCureentJob="Teacher";
+
+// let job1="Software Engineering";
+// let job2="Teacher";
+
+//DataTypes
+/*let jsIsCaseSensitive=true;
+console.log(jsIsCaseSensitive);
+console.log(typeof jsIsCaseSensitive);
+
+let y;
+console.log(y);
+console.log(typeof y);
+
+let dob=2005;
+console.log(typeof dob);
+
+let firstname="sneha";
+console.log(typeof firstname);
+
+let x=null;
+console.log(typeof x);*/
+
+//const var
+/*let data=60;
+data=100;
+
+ //not possible
+ const age=20;
+age=19;*/
+
+
+//operators
+/*const ageSneha=2024-2005;
+const ageNeha=2024-2004;
+console.log(ageSneha,ageNeha);
+console.log(ageNeha * 2 , ageSneha / 10,2**2);
+//2**2 means 2 the power of 2=2*2
+
+//concatenation
+const firstName="simran";
+const middleName="jeet";
+const lastName="kaur";
+console.log(firstName + " " +middleName + " " +lastName);
+
+//Assignment operator
+let x=30-5;
+x+=10; // x=x+10=35;
+x++; // 36
+x--; //35;
+console.log(x);
+
+// comparison operator
+console.log(ageNeha>ageSneha);
+
+const isFullAge =ageSneha>=21;
+console.log(isFullAge);*/
+
+// day 2
+// operator precendence
+/*const year=2024;
+const ageNeha=2024-2004;
+const ageSneha=2024-2005;
+console.log(year-1990>year-2020);
+ 
+let p,q;
+p=q=20-10-2; // p=q=8,p=8
+console.log(p,q);*/
+
+
+//string and template literals
+/*const name="priya";
+ const lastname="arora";
+ const job="student";
+ const birth="2005";
+ const year="2024";
+ const newText=`I am ${name} ${lastname} ${job},a ${year-birth} year old.`;
+ console.log(newText);
+console.log(`just a regular string.....`);
+console.log(`String
+                 is a
+                     collection of
+                              characters`);*/
+
+//if else
+/*const text=20;
+const line=text>=30;
+if(line){
+    console.log("bigger");
+}
+else{
+    console.log("smaller");
+}
+const birthyear=2005;
+let century;
+if(birthyear>=2000)
+{
+century=20;
+}else{
+    century=21;
+}
+console.log(century);*/
+
+//day 3
+//type conversion
+/*const year=1991;
+console.log(Number(year),year);
+console.log(Number(year)+18);
+console.log(Number("Riya"));
+console.log(typeof Nan);
+console.log(String(23),23);*/
+
+//type coercion
+/*console.log("I am" + 19 + " years old!");
+console.log("19"-"10"-3);
+console.log("10"/"2");
+
+let n="2"+1;
+n=n-1;
+console.log(n);
+
+console.log(2+3+4+"1");
+console.log("30" - "20" - "10" + 1 +"5");*/
+
+// 5 falsy values: 0, ' ', undefined,null,NaN
+/*console.log(Boolean(0));
+console.log(Boolean(""));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(NaN));
+console.log(Boolean({}));
+
+const rupees=0;
+if(rupees){
+    console.log("Spend all:")}
+    else{
+        console.log("don't spend ")
+    }
+    let day;
+    if(day)
+    {
+        console.log("amazing day");
+    }
+    else{
+        console.log("not amazing day")
+    }*/
+
+        //logical operator
+        /*const hasDrivingLicense=true;
+        const hasGoodVision=false;
+        console.log(hasDrivingLicense &&  hasGoodVision);
+        console.log(hasDrivingLicense || hasGoodVision);
+        console.log(! hasDrivingLicense );
+
+const shouldDrive =hasDrivingLicense && hasGoodVision;
+if(hasDrivingLicense && hasGoodVision)
+{
+    console.log("able to drive");
+}
+else{
+    console.log("not able to drive");
+}*/
+
+
+
+
+
